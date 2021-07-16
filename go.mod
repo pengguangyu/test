04ae811e7a27e63be135f329442a56f9852617d2
pengguangyu <pengguangyu@cls.cn>
2021-07-16 17:26:05 +0800

    copier 结构体之间赋值

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module test
 
 go 1.15
 
-require github.com/jinzhu/copier v0.3.2
+require (
+	github.com/jinzhu/copier v0.3.2 // indirect
+	github.com/ybzhanghx/copier v0.0.0-20201016031042-c365a74ca62d
+)
--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/copier"
+	"strconv"
+	"time"
+
+	// "github.com/jinzhu/copier"
+	"github.com/ybzhanghx/copier"
 	// "hub.fastgit.org/jinzhu/copier"
 )
 
 type User struct {
-	Name string
-	Role string
-	Age  int32
+	NameT string `json:"name"`
+	Role  string
+	Age   int32
 }
 
 func (user *User) DoubleAge() int32 {
@@ -17,7 +21,7 @@ func (user *User) DoubleAge() int32 {
 }
 
 type Employee struct {
-	Name      string
+	Name      string `json:"name"`
 	Age       int32
 	DoubleAge int32
 	EmployeId int64
@@ -29,14 +33,18 @@ func (employee *Employee) Role(role string) {
 }
 
 func main() {
+	CopyByTag2()
+
 	var (
-		user      = User{Name: "Jinzhu", Age: 18, Role: "Admin"}
-		users     = []User{{Name: "Jinzhu", Age: 18, Role: "Admin"}, {Name: "jinzhu 2", Age: 30, Role: "Dev"}}
+		user  = User{NameT: "Jinzhu", Age: 18, Role: "Admin"}
+		users = []User{{NameT: "Jinzhu", Age: 18, Role: "Admin"},
+			{NameT: "jinzhu 2", Age: 30, Role: "Dev"}}
 		employee  = Employee{}
 		employees = []Employee{}
 	)
 
-	copier.Copy(&employee, &user)
+	// copier.Copy(&employee, &user)
+	copier.CopyByTag(&employee, &user, "json")
 
 	fmt.Printf("%#v \n", employee)
 	// Employee{
@@ -48,7 +56,8 @@ func main() {
 	// }
 
 	// Copy struct to slice
-	copier.Copy(&employees, &user)
+	// copier.Copy(&employees, &user)
+	copier.CopyByTag(&employees, &user, "json")
 
 	fmt.Printf("%#v \n", employees)
 	// []Employee{
@@ -57,11 +66,53 @@ func main() {
 
 	// Copy slice to slice
 	employees = []Employee{}
-	copier.Copy(&employees, &users)
+	// copier.Copy(&employees, &users)
+	copier.CopyByTag(&employees, &users, "json")
 
 	fmt.Printf("%#v \n", employees)
 	// []Employee{
 	//   {Name: "Jinzhu", Age: 18, DoubleAge: 36, EmployeId: 0, SuperRule: "Super Admin"},
 	//   {Name: "jinzhu 2", Age: 30, DoubleAge: 60, EmployeId: 0, SuperRule: "Super Dev"},
 	// }
+}
+
+///////////////////////
+
+type structA struct {
+	Items   string `mson:"Item_string"`
+	UserId  string `mson:"UserId_string"`
+	PubTime int64  `mson:"PubTime_int64"`
+}
+
+type structB struct {
+	Item    string    `mson:"Item_string"`
+	UserId  int64     `mson:"UserId_int64"`
+	PubTime time.Time `mson:"PubTime_time_Time"`
+}
+
+func (p *structB) UserId_string(t string) {
+	p.UserId, _ = strconv.ParseInt(t, 10, 64)
+}
+
+func (p *structB) PubTime_int64(t int64) {
+	p.PubTime = time.Unix(t, 0)
+}
+
+func CopyByTag2() {
+	obj1 := structA{Items: "233", UserId: "5433", PubTime: time.Now().Unix()}
+	obj2 := &structB{}
+	err := copier.CopyByTag(obj2, &obj1, "mson")
+	if err != nil {
+		fmt.Println("Should not raise error")
+	}
+
+	if obj2.Item != obj1.Items {
+		fmt.Println("Field A should be copied")
+	}
+	if strconv.FormatInt(obj2.UserId, 10) != obj1.UserId {
+		fmt.Println("Field B should be copied")
+	}
+	if obj2.PubTime.Unix() != obj1.PubTime {
+		fmt.Println("Field C should be copied")
+	}
 }

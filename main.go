2658707325ef70590f7343ac1362cea0e59d663b
pengguangyu <pengguangyu@cls.cn>
2021-07-16 16:55:11 +0800

    fix

--- main.go
+++ main.go
@@ -4,17 +4,6 @@ import (
 	"fmt"
 )
 
-func JoinStrUseSprint(a, b string) string {
-	return fmt.Sprintf("%s%s", a, b)
-}
-
-func JoinStrUseNor(a, b string) string {
-	return a + b
-}
-
-// 运行所有的测试
-// go test -v -bench=.  main_test.go main.go
-// 运行所有的基准测试函数
-// go test -bench=. -benchtime=1s -benchmem -count=10
 func main() {
+	fmt.Println("----")
 }

cc461c3fa2bbe53fcb152bb5d4d997f1ae7e06e8
pengguangyu <pengguangyu@cls.cn>
2021-07-29 15:42:53 +0800

    fix 基准 性能测试 Benchmark

--- fib_test.go
+++ fib_test.go
@@ -1,11 +1,45 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 //           后缀是Fib的    每次1s         3轮      内存
 // go test -bench='Fib$' -benchtime=1s -count=3 -benchmem .
 func BenchmarkFib(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		fib(30) // run fib(30) b.N times
+	}
+}
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func generate(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func BenchmarkGenerateWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generateWithCap(1000000)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate(1000000)
 	}
 }
